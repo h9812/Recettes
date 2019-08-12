4cd56440946e44de05133b1711ee5b672975d51d
unknown <nguyentaido98@gmail.com>
2019-08-12 10:31:47 +0700

    create navibar

---
+++ app/src/main/java/com/d2h2/recettes/FragmentSearch.java
@@ -0,0 +1,34 @@
+package com.d2h2.recettes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class FragmentSearch extends Fragment {
+    private TextView mTxtSearch;
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView(view);
+        initAction();
+    }
+
+    private void initView(View view) {
+        mTxtSearch = view.findViewById(R.id.txt_search);
+    }
+
+    private void initAction() {
+        mTxtSearch.setText(R.string.title_search);
+    }
+}
--- app/src/main/java/com/d2h2/recettes/MainActivity.java
+++ app/src/main/java/com/d2h2/recettes/MainActivity.java
@@ -1,14 +1,55 @@
 package com.d2h2.recettes;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    FragmentHome fragmentHome = new FragmentHome();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragmentHome).commit();
+                    return true;
+                case R.id.navigation_search:
+                    FragmentSearch fragmentSearch = new FragmentSearch();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragmentSearch).commit();
+                    return true;
+                case R.id.navigation_profile:
+                    FragmentProfile fragmentProfile = new FragmentProfile();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragmentProfile).commit();
+
+                    return true;
+                case R.id.navigation_setting:
+                    FragmentSetting fragmentSetting = new FragmentSetting();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragmentSetting).commit();
+
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FragmentHome fragmentHome = new FragmentHome();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragmentHome).commit();
         setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 }
